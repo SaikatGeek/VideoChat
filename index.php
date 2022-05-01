36a63292e02974f1d4a7de943554f5b2120aab07
SaikatGeek <saikat.geek@gmail.com>
2022-05-01 23:57:32 +0600

    Socket server setup

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
 echo "hello world";
+
+?>
+
+<script>
+    var conn = new WebSocket('ws://localhost:8000');
+    conn.onopen = function(e) {
+        console.log("Connection established!");
+    };
+
+    conn.onmessage = function(e) {
+        console.log(e.data);
+    };
+</script>
+
